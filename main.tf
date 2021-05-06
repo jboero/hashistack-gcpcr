4500bee67592166862e683b0b9f45a6b60e16279
jboero <boeroboy@gmail.com>
2021-05-06 19:34:44 +0100

    Simplified TF with for_each

--- main.tf
+++ main.tf
@@ -1,11 +1,48 @@
+/************  John Boero - jboero@hashicorp.com
+ This sample uses a map variable and a for_each to deploy your container images
+ as Google Cloud Run singletons (limit 1 container).  TLS is provided.
+ You must customize your own image and build it into your own image registry.*/
 variable "proj" {
     description = "GCP Project ID."
-    default = "gb-playground"
+    default = "{YOURPROJ}"
 }
 
 variable "location" {
-    description = "GCP Region to deploy cloud run services - ie eu-west4."
+    description = "GCP Region to deploy cloud run services - ie europe-west4."
     default = "europe-west4"
+}
+
+// Set this to a map of your container images and which primary port they use.
+// Since it's a map you can add extra attributes if you need to.
+variable "singletons" {
+  description = "Map of containers and ports to deploy in GCR."
+  default = {
+    vault = {
+      name    = "vault:latest"
+      command = ["/usr/bin/vault", "server", "-dev"]
+      port    = 8200
+    },
+    consule = {
+      name  = "consul:latest"
+      command = ["/usr/bin/consul", "agent", "-dev"]
+      port = 8500
+    },
+    nomad = {
+      name  = "nomad:latest"
+      command = ["/usr/bin/nomad", "agent", "-dev"]
+      port = 4646
+    },
+    waypoint = {
+      name  = "waypoint:latest"
+      command = ["/usr/bin/waypoint", "server", "run", "-accept-tos"]
+      port = 9702
+    }/* ,
+    boundary = { // Boundary requires an external Postgres db.
+      name  = "boundary:latest"
+      command = ["/usr/bin/boundary", "server", "-config=/etc/boundary.d/controller.hcl"]
+      port = 9200
+    }*/
+  }
 }
 
 provider "google" {
@@ -23,19 +60,21 @@ data "google_iam_policy" "noauth" {
   }
 }
 
-resource "google_cloud_run_service" "nomad-gcr" {
-  name     = "nomad-gcr"
+// Use a for_each on var.singletons
+resource "google_cloud_run_service" "runners" {
+  for_each = var.singletons
+  name     = "${each.key}-gcr"
   location = var.location
   project  = var.proj
   
   template {
     spec {
       containers {
-        image = "gcr.io/${var.proj}/nomad:1.0.4"
-        //command = ["/usr/bin/nomad", "agent", "-dev"]
+        image = "gcr.io/${var.proj}/${each.value.name}"
+        command = each.value.command
         ports {
             name = "http1"
-            container_port = 4646
+            container_port = each.value.port
         }
       }
     }
@@ -44,95 +83,22 @@ resource "google_cloud_run_service" "nomad-gcr" {
       annotations = {
         "autoscaling.knative.dev/maxScale"      = "1"
         "autoscaling.knative.dev/minScale"      = "1"
-        "run.googleapis.com/client-name"        = "terraform"
-      }
-    }
-    
-  }
-  //autogenerate_revision_name = true
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth-nomad" {
-  location    = var.location
-  project     = var.proj
-  service     = google_cloud_run_service.nomad-gcr.name
-  policy_data = data.google_iam_policy.noauth.policy_data
-}
-
-resource "google_cloud_run_service" "vault-gcr" {
-  name     = "vault-gcr"
-  location = var.location
-  project  = var.proj
-  
-  template {
-    spec {
-      containers {
-        image = "gcr.io/${var.proj}/vault:1.6.3"
-        ports {
-            name = "http1"
-            container_port = 8200
-        }
-      }
-    }
-
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale"      = "1"
-        "autoscaling.knative.dev/minScale"      = "1"
-        "run.googleapis.com/client-name"        = "terraform"
+        "run.googleapis.com/client-name"        = each.value.name
       }
     }
   }
 }
 
-resource "google_cloud_run_service_iam_policy" "noauth-vault" {
+// This is ugly in prod but for demos open up noauth access to outsiders.
+resource "google_cloud_run_service_iam_policy" "noauths" {
+  for_each    = google_cloud_run_service.runners
   location    = var.location
   project     = var.proj
-  service     = google_cloud_run_service.vault-gcr.name
+  service     = each.value.name
   policy_data = data.google_iam_policy.noauth.policy_data
 }
 
-resource "google_cloud_run_service" "consul-gcr" {
-  name     = "consul-gcr"
-  location = var.location
-  project  = var.proj
-  
-  template {
-    spec {
-      containers {
-        image = "gcr.io/${var.proj}/consul:1.9.4"
-        ports {
-            name = "http1"
-            container_port = 8500
-        }
-      }
-    }
-
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale"      = "1"
-        "autoscaling.knative.dev/minScale"      = "1"
-        "run.googleapis.com/client-name"        = "terraform"
-      }
-    }
-  }
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth-consul" {
-  location    = var.location
-  project     = var.proj
-  service     = google_cloud_run_service.consul-gcr.name
-  policy_data = data.google_iam_policy.noauth.policy_data
-}
-
-output "endpoint-consul" {
-  value = google_cloud_run_service.consul-gcr.status[0].url
-}
-
-output "endpoint-nomad" {
-  value = google_cloud_run_service.nomad-gcr.status[0].url
-}
-
-output "endpoint-vault" {
-  value = google_cloud_run_service.vault-gcr.status[0].url
+// Output all of our endpoints to people can consume them in other workspaces.
+output "endpoints" {
+  value = toset([for u in google_cloud_run_service.runners : u.status[0].url])
 }
