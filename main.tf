de63ca91e56f1a8c9c4b9b9ea9dd182aae2c241c
JohnnyB <jboero@users.noreply.github.com>
2021-05-11 08:42:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,51 +2,6 @@
  This sample uses a map variable and a for_each to deploy your container images
  as Google Cloud Run singletons (limit 1 container).  TLS is provided.
  You must customize your own image and build it into your own image registry.*/
-<<<<<<< HEAD
-=======
-variable "proj" {
-    description = "GCP Project ID."
-    default = "{YOURPROJECT}"
-}
-
-variable "location" {
-    description = "GCP Region to deploy cloud run services - ie europe-west4."
-    default = "europe-west4"
-}
-
-// Set this to a map of your container images and which primary port they use.
-// Since it's a map you can add extra attributes if you need to.
-variable "singletons" {
-  description = "Map of containers and ports to deploy in GCR."
-  default = {
-    vault = {
-      name    = "vault:latest"
-      command = ["/bin/vault", "server", "-dev", "-dev-listen-address=:8200"]
-      port    = 8200
-    },
-    consul = {
-      name  = "consul:latest"
-      command = ["/bin/consul", "agent", "-config-dir=/etc/consul.d"]
-      port = 8500
-    },
-    nomad = {
-      name  = "nomad:latest"
-      command = ["/usr/bin/nomad", "agent", "-dev", "-bind", "0.0.0.0"]
-      port = 4646
-    },
-    waypoint = {
-      name  = "waypoint:latest"
-      command = ["/usr/bin/waypoint", "server", "run", "-accept-tos", "-listen-http=0.0.0.0:9702", "-db=/tmp/tmp.db"]
-      port = 9702
-    },/*
-    boundary = { // Boundary requires an external Postgres db.
-      name  = "boundary:latest"
-      command = ["/bin/boundary", "server", "-config=/etc/boundary.d/controller.hcl"]
-      port = 9200
-    }*/
-  }
-}
->>>>>>> 54a34d8a55844675f9f7e7fc7ea823e14f068ba3
 
 provider "google" {
   project = var.proj
